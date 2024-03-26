8e96c13e7ca2dd823adc8f06b68c18da1081f854
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-03-26 16:49:53 +0100

    first commit

---
+++ src/types/buttons.types.ts
@@ -0,0 +1,12 @@
+
+
+export type ShareBtnTextType = {
+   onClick: () => void
+}
+export type SaveBtnTextType = {
+   HandleOnClick: () => void
+}
+export type SeeMoreTextType = {
+   HandleOnClick: () => void,
+   value?: boolean
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens:{
+        "custom-lg": "1128px"
+      },
+      boxShadow: {
+        "customShadow": 'rgba(0, 0, 0, 0.12) 0px 2px 16px 0px'
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
